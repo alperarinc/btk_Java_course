c3b5cb3614c3b3caf2214f2f83841f20d1b4fc53
redoxkali <alperarinc@hotmail.com>
2022-01-19 22:20:12 +0300

    Java Encapsulation

---
+++ javademos/classesWithAttributes/src/ProductManager.java
@@ -0,0 +1,13 @@
+public class ProductManager {
+    public void Add(Product product){
+        //JDBC //Bu alan database yazdırma alanı //*ekle *sil *güncelle *arama
+        System.out.println("Ürün Eklendi :)  " + product.getName());
+    }
+
+
+    public void Delete(Product product){
+
+
+        System.out.println("Ürün Silindi :)  " + product.getName());
+    }
+}
