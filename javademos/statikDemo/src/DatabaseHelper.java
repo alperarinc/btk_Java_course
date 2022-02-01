7c98f397548109dd38c3969067fd6c3bad97afdf
redoxkali <alperarinc@hotmail.com>
2022-02-01 22:33:55 +0300

    Java Inner Class & Static

---
+++ javademos/statikDemo/src/DatabaseHelper.java
@@ -0,0 +1,16 @@
+public class DatabaseHelper {
+    public class Crud{ //Create read update delete
+        public static void Delete(){
+
+        }
+        public class Update{
+
+        }
+
+    }
+    public static class Connection{
+        public static void createConnection(){
+
+        }
+    }
+}
